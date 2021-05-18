3dcaa1b6729665655b53467fca1a989c3df31bab
lauracarlotta <carlotta.custodio@gmail.com>
2021-05-18 00:04:32 -0300

    add event listener to form

---
+++ form/form.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("Pode enviar o formulaŕoio");
+
+    // não vai enviar o formulaŕio
+    event.preventDefault();
+})
